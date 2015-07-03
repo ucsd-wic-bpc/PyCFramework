e9cad8a3f29f806b273c8cb4cda5c8c104dcd9cb
Brandon Milton <brandonio21@linux.com>
2015-07-02 22:33:11 -0700

    Created test file

---
+++ Solutions/dev/test.py
@@ -0,0 +1,13 @@
+################################################################################
+# Filename: test.py
+# Author:   Brandon Milton
+#           http://brandonio21.com
+# Date:     July 2, 2015
+# 
+# This file serves as a replacement for test.sh. It compiles and runs all 
+# user inputted solutions against test cases.
+#
+# Usage:
+#   $ python test.py <problem> <user>
+#   
+################################################################################
