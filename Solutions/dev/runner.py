e519406fbe6c7362eb1df75e67ae1f3880412805
Brandon Milton <brandonio21@linux.com>
2015-09-24 10:44:09 -0700

    Added more user story comments, initial tests, runner

---
+++ Solutions/dev/runner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+################################################################################
+# Filename: runner.py
+# Author:   Brandon Milton, http://brandonio21.com
+# Date:     24 September 2015
+#
+# Contains the executable logic for PyCFramework
+################################################################################
+
--- Solutions/dev/tests/test_runner.py
+++ Solutions/dev/tests/test_runner.py
@@ -6,15 +6,24 @@
 # Contains tests for runner.py, the main executable script
 ################################################################################
 import unittest
-
+import runner
+from io import StringIO
 
 class TestRunner(unittest.TestCase):
 
     # USER STORY:
     # Mary wants to utilize PyCFramework. She doesnt know where to begin.
     # She types ./runner.py without arguments in hopes of a help message
-    # Mary may also be advanced on some days, where she types -h for help
+    def test_can_be_run_directly(self):
+        with open('runner.py') as openRunner:
+            self.assertEquals(openRunner.readline()[:-1], '#!/usr/bin/env python3')
 
+    def test_does_return_help_message_no_args(self):
+        out = StringIO()
+        pass
+
+
+    # Mary may also be advanced on some days, where she types -h for help
 
     # After learning how to use the software, she wants to see the problems that
     # she has completed. However, she has not yet created her user. 
@@ -34,3 +43,14 @@ class TestRunner(unittest.TestCase):
 
     # Note that Mary should later be able to delete herself with
     # ./runner.py --deleteWriter Mary
+
+    # Now Mary wants to test a single problem that she has completed
+    # ./runner.py Mary 5
+    # The problem is incorrect, so Mary receives a message indicating so
+    # $ Incorrect Solution: Mary 5 Corner-Case
+
+    # Mary wants to see the specifics of how she failed, so she runs with the 
+    # --diff option
+    # ./runner.py Mary 5 --diff
+    # $ Incorrect Solution: Mary 5 Corner-Case
+    #   <diff output>
