0ec0c3fed4f0ada13f89f76f33606e7fc44aa7ed
Brandon Milton <brandonio21@linux.com>
2016-01-07 15:14:24 -0800

    Initial cleanup of package subparser

--- Solutions/dev/util/subparsers/package.py
+++ Solutions/dev/util/subparsers/package.py
@@ -53,6 +53,20 @@ def load_config_file(path=None):
     global config
     config = fileops.get_json_dict(path)
 
+def _generate_case_naming_dict(caseName: str, caseNumber: int):
+    return { 'caseType' : caseName, 'number'   : caseNumber }
+
+def _generate_case_naming_dict_with_io(ioType: str, caseName: str, caseNumber: int):
+    namingDict = _generate_case_naming_dict(caseName, caseNumber)
+    namingDict['input_output'] = ioType
+    return namingDict
+
+def _generate_input_case_naming_dict(caseName: str, caseNumber: int):
+    return _generate_case_naming_dict_with_io('input', caseName, caseNumber)
+
+def _generate_output_case_naming_dict(caseName: str, caseNumber: int):
+    return _generate_case_naming_dict_with_io('output', caseName, caseNumber)
+
 def package_case(caseName: str, caseDir: str, cases: list, namingScheme:str):
     inputPath = fileops.join_path(caseDir, 'input')
     outputPath = fileops.join_path(caseDir, 'output')
@@ -60,35 +74,45 @@ def package_case(caseName: str, caseDir: str, cases: list, namingScheme:str):
     fileops.make(outputPath, FileType.DIRECTORY)
     for case in cases:
         if case.get_case_string() == caseName:
-            inputNamingDict = {
-                    'input_output' : 'input',
-                    'caseType' : caseName,
-                    'number' : case.caseNumber
-                    }
-            outputNamingDict = {
-                    'input_output' : 'output',
-                    'caseType' : caseName,
-                    'number' : case.caseNumber
-                    }
+            inputNamingDict  = _generate_input_case_naming_dict(caseName, case.caseNumber)
+            outputNamingDict = _generate_output_case_naming_dict(caseName, case.caseNumber)
+
             inputFilePath = fileops.join_path(inputPath, namingScheme.format(**inputNamingDict))
             outputFilePath = fileops.join_path(outputPath, namingScheme.format(**outputNamingDict))
+
             fileops.write_file(inputFilePath, case.inputContents)
             fileops.write_file(outputFilePath, case.outputContents)
 
 def compress_case(caseName: str, casePath: str, compressionDict: dict):
-    print("CASE")
+    if compressionDict is None: return
+
     if compressionDict['enabled']:
         if compressionDict['method'] == 'zip':
             fileops.zipdir(casePath, '{}.zip'.format(casePath))
 
 def package_type(packagePath: str, cases: list, packageType: dict, compressionDict: dict):
-    print("TYPE")
     for case in packageType['cases']:
-        print("AHHH")
         casePath = fileops.join_path(packagePath, case)
         fileops.make(casePath, FileType.DIRECTORY)
         package_case(case, casePath, cases, packageType['naming'])
         compress_case(case, casePath, compressionDict)
+
+def _get_compression_from_layout_dict(layoutDict: dict):
+    if COMPRESSION_KEYWORD in layoutDict:
+        return layoutDict[COMPRESSION_KEYWORD]
+    else:
+        return None
+
+def package_into_path(path: str, layoutDict: dict):
+    for problemNumber, caseList in case.get_all_cases().items():
+        problemDirName = Definitions.get_value('solution_naming').format(
+                **dict(problem=problemNumber))
+        problemPath = fileops.join_path(path, problemDirName)
+        fileops.make(problemPath, FileType.DIRECTORY)
+
+        for packageTypeName, packageType in layoutDict[TYPES_KEY].items():
+            package_type(problemPath, caseList, packageType, 
+                _get_compression_from_layout_dict(layoutDict))
 
 def package(savePaths: list, configFilePath: str=None, layout: str=None):
     load_config_file(path=configFilePath)
@@ -97,17 +121,10 @@ def package(savePaths: list, configFilePath: str=None, layout: str=None):
     # Ensure layout is provided
     if len(config) > 1 and layout is None:
         raise Exception('Error: Must choose a package layout using --layout')
-    if not layout in config:
+    if len(config) > 1 and not layout in config:
         raise Exception('Error: {} is an invalid layout'.format(layout))
 
+    if len(config) == 1: layout = list(config.keys())[0]
     for path in savePaths:
         fileops.make(path, FileType.DIRECTORY)
-
-        for problemNumber, caseList in case.get_all_cases().items():
-            problemDirName = Definitions.get_value('solution_naming').format(
-                    **{'problem' : problemNumber})
-            problemPath = fileops.join_path(path, problemDirName)
-            fileops.make(problemPath, FileType.DIRECTORY)
-            for packageTypeName, packageType in config[layout][TYPES_KEY].items():
-                package_type(problemPath, caseList, packageType, 
-                    config[layout][COMPRESSION_KEYWORD])
+        package_into_path(path, config[layout])
