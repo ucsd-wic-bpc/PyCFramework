d2932d67d2984a7c4c249599fa1a08f3324a3728
Brandon Milton <brandon.milton21@gmail.com>
2016-03-09 19:36:10 -0800

    Merged import functionality into add module. Import functionality now deprecated

--- Solutions/dev/util/subparsers/subparsers/writersadd.py
+++ Solutions/dev/util/subparsers/subparsers/writersadd.py
@@ -9,6 +9,7 @@
 from util.writer import Writers, Writer
 from util.perror import PyCException
 from util.pathmapper import PathMapper
+from util import fileops
 import sys
 SUBPARSER_KEYWORD = 'add'
 
@@ -22,9 +23,16 @@ def operate(args):
     Arguments:
     args: Namespace - The arguments passed via CLI
     """
+    # If the user specified 'fromcsvs', only add from the CSV files
+    if not args.csvfilepaths is None and len(args.csvfilepaths) > 0:
+        for filepath in args.csvfilepaths[0]:
+            import_writers_from_csv_file_path(filepath)
+
+        return
+
     # Should we launch interactive mode?
     if args.writer_names is None or len(args.writer_names) == 0:
-        add_users_interactively()
+        add_users_interactively(showhint = args.showhint)
     elif len(args.writer_names) == 1:
         add_user_quick(args.writer_names[0])
         #TODO: Delegate to the EDIT subparser to edit this user with details
@@ -49,6 +57,9 @@ def add_to_subparser_object(subparserObject, parentParser):
     # Add the writer names positional. If this turns out to be an empty list,
     # interactive mode will be launched. Else, the given writers will be created
     addParser.add_argument('writer_names', nargs='*')
+    addParser.add_argument('--fromcsvs', action='append', dest='csvfilepaths',
+            nargs='+')
+    addParser.add_argument('--showhint', action='store_true')
     addParser.set_defaults(func=operate)
 
 def add_user_quick(userName: str):
@@ -87,13 +98,50 @@ def add_users_quick(userList: list):
     if len(invalidUsers) > 0:
         raise PyCException('Error: Writers {} already exist'.format(invalidUsers))
 
-def add_users_interactively():
+def add_users_interactively(showhint=False):
     """
     Adds many users by allowing the user to interactively create information
     in the same way that it would be parsed from the CSV file using "import".
     """
-    print(("Enter user information, one per line, in the following template:\n"
-        'folder,"full name","email","language1,language2"'))
-    userInput = sys.stdin.read()
-    # TODO: Delegate to import module
-    print(userInput)
+    if showhint:
+        print(("Enter user information, one per line, in the following template:\n"
+            'folder,"full name","email","language1,language2"'))
+    import_writers_from_csv_descriptor(sys.stdin)
+
+def import_writers_from_csv_file_path(csvFilePath):
+    """
+    Imports the writers from a CSV file path
+    """
+    realPath = PathMapper.get_mapped_path(csvFilePath)
+    with open(realPath, 'r') as csvFileDescriptor:
+        import_writers_from_csv_descriptor(csvFileDescriptor)
+
+def import_writers_from_csv_descriptor(csvFileDescriptor):
+    """
+    Imports the writers from a CSV file descriptor
+    """
+    writerDetailsList = fileops.parse_csv_from_file_descriptor(csvFileDescriptor)
+    for individualWriterDataChunk in writerDetailsList:
+        _create_writer_from_list(individualWriterDataChunk)
+
+def _create_writer_from_list(datalist: list):
+    # TODO: Change CSV implementation to use dict reader for better handling of 
+    # missing information
+    """
+    Private function. Creates a single writer from a list following the format:
+
+    ["folder","name","email","language1,language2"]
+
+    Arguments:
+    datalist: list - The list of data to load into the writer
+    """
+    # First, verify that the data list is OK
+    if datalist is None or not len(datalist) == 4:
+        raise PyCException('Cannot create writer from datalist {}. Malformed'
+                .format(str(datalist)))
+
+    newWriter = Writer(writerPath=datalist[0],writerName=datalist[1],
+            writerEmail=datalist[2])
+    newWriter.create()
+    splitLanguageList = [languageName.strip() for languageName in datalist[3].split(',')]
+    newWriter.add_known_language_from_list(splitLanguageList)
