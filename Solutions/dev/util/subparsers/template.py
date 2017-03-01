57ef957ae84146a81a193e43dee7a470293e9895
Brandon Milton <brandon.milton21@gmail.com>
2017-03-01 12:51:53 -0800

    Incremental improvements in Java template generation

--- Solutions/dev/util/subparsers/template.py
+++ Solutions/dev/util/subparsers/template.py
@@ -19,7 +19,7 @@ import json
 from util.pathmapper import PathMapper
 from util import case, fileops
 from util.fileops import FileType, get_files_in_dir, join_path, get_json_dict, get_path_with_changed_extension
-from util.case import Case
+from util.case import Case, get_all_cases
 from util.definitions import Definitions
 from util.language import Languages
 
@@ -34,8 +34,7 @@ def operate(args):
     Arguments:
     args: Namespace - The arguments passed via CLI
     """
-    generate_templates_from_case_files(args.case_files, args.language, 
-            args.output)
+    generate_templates_from_case_files(args.problems, args.language, args.output)
 
 def add_to_subparser_object(subparserObject, parentParser):
     """
@@ -51,38 +50,112 @@ def add_to_subparser_object(subparserObject, parentParser):
     templateParser = subparserObject.add_parser(SUBPARSER_KEYWORD, 
                                                 parents=[parentParser])
     defaultTemplateLoc = PathMapper.get_mapped_path_from_parent('Templates','Generated')
-    templateParser.add_argument('case_files', nargs='+')
     templateParser.add_argument('--output', default=defaultTemplateLoc)
     templateParser.set_defaults(func=operate)
 
 
-def generate_templates_from_case_files(caseFilePaths:list, languages: list,
+def generate_templates_from_case_files(problems:list, languages: list,
         outputPath: str):
     """
     Attempts to generate template files for the given case files.
     """
-    for path in caseFilePaths:
-        caseList = case.get_cases_from_json_file(path)
+    for problem in problems:
+        caseList = get_all_cases(problemNumber=problem)[int(problem)]
         if languages is None or len(languages) == 0:
             languages = Languages.get_all_language_names()
-        generate_template_for_case_collection(caseList, languages, outputPath)
+        generate_template_for_case_collection(caseList, languages, outputPath, problem)
 
-def generate_template_for_case_collection(cases: list, languages: list, outputPath: str):
+def get_jsonstr(jsonstr):
+    """
+    Return the string associated with the json object. If the json object is
+    a list, the list is joined with newlines
+    """
+    if isinstance(jsonstr, list):
+        return '\n'.join(jsonstr)
+    else:
+        return jsonstr
+
+def get_type_from_many(jsonstrs):
+    strtypes = [get_type(jsonstr) for jsonstr in jsonstrs]
+
+    finalized_types = []
+    for i in range(1, len(strtypes)):
+        if strtypes[i-1] == None or strtypes[i] == None:
+            continue
+
+        # What if types dont match?
+        if strtypes[i-1] != strtypes[i]:
+            if "char" in strtypes[i-1] and "str" in strtypes[i]:
+                strtypes[i-1] = strtypes[i]
+                finalized_types.append(strtypes[i-1])
+            elif "str" in strtypes[i-1] and "char" in strtypes[i]:
+                strtypes[i] = strtypes[i-1]
+                finalized_types.append(strtypes[i-1])
+            else:
+                print("ERROR!")
+        else:
+            finalized_types.append(strtypes[i-1])
+
+
+    return finalized_types[0]
+
+def get_individual_type(parsed_jsonstr):
+    if isinstance(parsed_jsonstr, str) and len(parsed_jsonstr) == 1:
+        return 'char'
+    else:
+        return type(parsed_jsonstr).__name__
+
+def get_type(jsonstr):
+    """
+    Return the type of the provided string
+    """
+    try:
+        parsed_jsonstr = json.loads(jsonstr)
+    except Exception:
+        parsed_jsonstr = jsonstr
+
+    type_string = get_individual_type(parsed_jsonstr)
+
+    list_depth = 0
+    while isinstance(parsed_jsonstr, list):
+        if len(parsed_jsonstr) <= 0:
+            return None
+
+        list_depth += 1
+        parsed_jsonstr = parsed_jsonstr[0]
+        type_string = get_individual_type(parsed_jsonstr)
+
+    if list_depth > 0:
+        type_string = "_".join((["list"] * list_depth) + [type_string])
+
+    return type_string
+
+
+def generate_template_for_case_collection(cases: list, languages: list, outputPath: str, problem):
     # First, let's populate a dictionary with all relevant information about
     # the cases. Let's just use the first case.
-    case = cases[0]
+
     language = Languages.get_language_by_name(languages[0])
 
-    outputContents = json.loads(case.outputContents)
-    inputContents = json.loads(case.inputContents)
-    outputType = type(outputContents).__name__
-    inputTypes = [type(i).__name__ for i in inputContents]
-    data = get_json_dict('data/problem{}.json'.format(case.problemNumber))
+    outputType = get_type_from_many([case.outputContents for case in cases])
+
+    # A list of input groups such that input_groups[0] is a list of all the first inputs
+    input_groups = []
+    for case in cases:
+        for i, input in enumerate(json.loads(case.inputContents)):
+            if len(input_groups) <= i:
+                input_groups.append([])
+
+            input_groups[i].append(input)
+
+    inputTypes = [get_type_from_many(inputs) for inputs in input_groups]
+
+    data = get_json_dict('cases/problem{}_data.json'.format(problem))
     wordNums = data["args"]
     template = get_json_dict('conf/templates/{}.json'.format(language.name))
 
     info_dictionary = {
-        'problem': case.problemNumber,
+        'problem': problem,
         'returnType': template["types"][outputType],
         'defaultReturnType': template["default_values"][outputType],
         'methodName': data["method"],
@@ -116,70 +189,10 @@ def generate_template_for_case_collection(cases: list, languages: list, outputPa
 
     for i, j in enumerate(inputTypes):
         info = {"type": template["types"][j], "varName": wordNums[i], "argNum":i}
-        print('{}{}'.format(gstr(template["main"]["decl"].format(**info)),
-                            gstr(template["args"][j].format(**info))))
+        print(gstr(template["args"][j]).format(**info))
 
-    outputdict = {'type': info_dictionary["returnType"], 'varName': 'output'}
-    print('{}{}'.format(gstr(template["main"]["decl"].format(**outputdict)),
-                        gstr(template["main"]["store_output"].format(**info_dictionary))))
-    print(gstr(template["main"]["print_output"].format(**info_dictionary)))
-    print(gstr(template["main"]["footer"].format(**info_dictionary)))
-    print(gstr(template["file"]["footer"].format(**info_dictionary)))
+    print(gstr(template["main"]["store_output"]).format(**info_dictionary))
+    print(gstr(template["main"]["print_output"]).format(**info_dictionary))
+    print(gstr(template["main"]["footer"]).format(**info_dictionary))
+    print(gstr(template["file"]["footer"]).format(**info_dictionary))
     return
-
-
-    for (templatePath, templateLanguage) in generate_blank_template_files_for_case_collection(cases, languages, outputPath):
-        fill_template_file_for_case_collection(templatePath, templateLanguage,
-                cases)
-
-def get_unformatted_template(language):
-    """
-    Returns the unformatted template text for the given Language object.
-    """
-    path = 'conf/templates/{}'.format(language.name)
-    return fileops.read_file(path)
-
-def generate_blank_template_files_for_case_collection(cases:list, languages:list,
-        outputPath: str):
-    """
-    Creates the template files in the given directory and yields the following 
-    tuple:
-    (path, Language)
-    so that they may be written to.
-    """
-    if cases is None or len(cases) == 0:
-        return []
-    else:
-        templateFile = Definitions.get_value('solution_naming').format(
-                **{'problem':cases[0].problemNumber})
-        templateFilePath = fileops.join_path(outputPath, '{}.{{}}'.
-                format(templateFile))
-
-        for languageName in languages:
-            languageObj = Languages.get_language_by_name(languageName)
-            path = templateFilePath.format(languageObj.get_extension())
-            fileops.make(path, FileType.FILE)
-            yield (path, languageObj)
-
-def get_language_specific_type(language, typeStr: str):
-    global templateDictionary
-    if templateDictionary is None:
-        dictionaryConfigPath = PathMapper.get_mapped_config_path('templates','dictionary.json')
-        templateDictionary = fileops.get_json_dict(dictionaryConfigPath)
-
-    return templateDictionary[language.name][typeStr]
-
-def output_type_to_proper_str(outputStr: str):
-    return type(json.loads(outputStr.lower())).__name__
-
-def fill_template_file_for_case_collection(path: str, language, cases: list):
-    formattedTemplate = get_unformatted_template(language).format(**{
-        'problem' : cases[0].problemNumber,
-        'returnType' : get_language_specific_type(language, 
-                       output_type_to_proper_str(cases[0].outputContents)),
-        'methodName' : "balalkjfslkafj",
-        'argList' : "int poop",
-        'defaultReturn' : 0,
-        })
-
-    fileops.write_file(path, formattedTemplate)
